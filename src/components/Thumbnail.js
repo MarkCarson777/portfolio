061402e0b0f63e8a18775779a015f48b71e3f420
Mark Carson <markcarson121@gmail.com>
2021-10-05 17:48:53 +0100

    created Thumbnail component

---
+++ src/components/Thumbnail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Thumbnail(props) {
+  return (
+    <div>
+      Thumbnail
+    </div>
+  )
+};
+
+export default Thumbnail;
