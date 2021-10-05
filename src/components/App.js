a929b98f03f0c40f67f86a5830d78dbec3712f08
Mark Carson <markcarson121@gmail.com>
2021-10-05 17:39:39 +0100

    added router setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './Projects.js';
+import Articles from './Articles.js';
+import About from './About.js';
 
 function App() {
   return (
-    <div className="App">
-      <div className="Navigation">
-        {/* <img src={} className="logo" alt="Logo Image" /> */}
-        <div className="navigation-sub">
-          <a href="" className="item">Projects</a>
-          <a href="" className="item">Articles</a>
-          <a href="" className="item">About</a>
+    <BrowserRouter>
+      <div className="App">
+
+        {/* router setup */}
+        <Route exact path="/" component={Projects} />
+        <Route exact path="/articles" component={Articles} />
+        <Route exact path="/about" component={About} />
+
+        <div className="Navigation">
+          {/* <img src={} className="logo" alt="Logo Image" /> */}
+          <div className="navigation-sub">
+            <a href="" className="item">Projects</a>
+            <a href="" className="item">Articles</a>
+            <a href="" className="item">About</a>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   )
 };
 
