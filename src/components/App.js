ca2732f60d96750f255966332339e8224ea947ce
Mark Carson <markcarson121@gmail.com>
2021-10-05 17:34:46 +0100

    added basic nav setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      Homepage
+    <div className="App">
+      <div className="Navigation">
+        {/* <img src={} className="logo" alt="Logo Image" /> */}
+        <div className="navigation-sub">
+          <a href="" className="item">Projects</a>
+          <a href="" className="item">Articles</a>
+          <a href="" className="item">About</a>
+        </div>
+      </div>
     </div>
   )
 };
