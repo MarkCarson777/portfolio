ae52b9bd0740c3e7984c6866e32596bf9234c3d4
Mark Carson <markcarson121@gmail.com>
2021-10-07 17:24:20 +0100

    made thumbnails responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,29 +3,32 @@ import Thumbnail from "./Thumbnail.js";
 import clock from "../assets/clock.jpg";
 import pokemon from "../assets/pokemon.jpg";
 import cocktail from "../assets/cocktail.jpg";
+import "../css/Projects.css";
 
 function Projects(props) {
   return (
     <div>
       <h1>Projects</h1>
-      <Thumbnail
-        link="https://www.righttime.club/"
-        image={clock}
-        title="RightTime"
-        // category="Website"
-      />
-      <Thumbnail
-        link="https://pikachoose2.herokuapp.com/"
-        image={pokemon}
-        title="Pikachoose"
-        // category="Website"
-      />
-      <Thumbnail
-        link="https://www.bournemouthbars.com/"
-        image={cocktail}
-        title="Bournemouth Bars"
-        // category="Website"
-      />
+      <div className="thumbnail-container">
+        <Thumbnail
+          link="https://www.righttime.club/"
+          image={clock}
+          title="RightTime"
+          // category="Website"
+        />
+        <Thumbnail
+          link="https://pikachoose2.herokuapp.com/"
+          image={pokemon}
+          title="Pikachoose"
+          // category="Website"
+        />
+        <Thumbnail
+          link="https://www.bournemouthbars.com/"
+          image={cocktail}
+          title="Bournemouth Bars"
+          // category="Website"
+        />
+      </div>
     </div>
   )
 };
