addc342310e88d461367314346ba6b7aa44a62a5
Mark Carson <markcarson121@gmail.com>
2021-10-07 17:40:14 +0100

    setup template righttime project page

--- src/components/Righttime.js
+++ src/components/Righttime.js
@@ -1,8 +1,17 @@
 import React from "react";
+import clock from "../assets/clock.jpg";
+import "../css/Righttime.css";
 
 function Righttime(props) {
   return (
-    <div>Hello</div>
+    <div>
+      <a className="image-container" href="https://www.righttime.club/">
+        <img className="project-link" src={clock} alt="Project Link" />
+      </a>
+      <div className="righttime-header">
+        <h1>RightTime</h1>
+      </div>
+    </div>
   )
 };
 
