e38c17321644b567d7e65ce12de5b5c386435538
Mark Carson <markcarson121@gmail.com>
2021-10-08 17:31:24 +0100

    fleshed out about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,29 @@ import React from "react";
 
 function About(props) {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section id="about">
+      <div>
+        <h1>
+          Hi , I'm Mark.
+          <br />I love to build amazing apps
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+          laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+          Laborum, voluptas natus?
+        </p>
+        <div>
+          <a
+            href="/contact">
+            Work with me  
+          </a>
+          <a
+            href="/projects">
+            See my work  
+          </a>
+        </div>
+      </div>
+    </section>
   )
 };
 
