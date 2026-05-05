b0fa15471d4d859bfed8cf058496fc670cbb76c9
Mark Carson <markcarson121@gmail.com>
2026-05-05 18:44:55 +0100

    installed cn and updated Button component

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,8 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Button } from ".";
 
-export default {
+const meta: Meta<typeof Button> = {
   title: "Button",
   component: Button,
+  args: {
+    children: "Click Me",
+  },
+  argTypes: {
+    onClick: { action: "onClick" },
+  },
 };
+export default meta;
 
-export const Default = () => <Button />;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {};
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,25 @@
-export const Button = () => {
-  return <button className="bg-blue-500">Click Me</button>;
+import { cn } from "@sglara/cn";
+
+type ButtonProps = {
+  disabled?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  disabled,
+  onClick,
+  children,
+}) => {
+  return (
+    <button
+      className={cn(
+        "bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600",
+        disabled && "opacity-50 cursor-not-allowed",
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
